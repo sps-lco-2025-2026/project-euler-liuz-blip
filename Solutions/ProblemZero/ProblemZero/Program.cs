6ea7b17c6b035384f3f2cf434b0be48c05fa9354
Jerry <liuz@stpaulsschool.org.uk>
2026-03-15 19:24:22 +0000

    ProjectEuler

---
+++ Solutions/ProblemZero/ProblemZero/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+class Program
+{
+    static void Main()
+    {
+        
+        sums = 0;
+        oddeven = 1;
+
+        for (int i = 1; i < 764; i++)
+        {
+            if (oddeven == 1)
+            {
+                sums += i * i;
+                oddeven += -1;
+            } else if (oddeven == -1)
+            {
+                oddeven += -1;
+                continue;
+            }
+        }
+    }
+}
+
+
